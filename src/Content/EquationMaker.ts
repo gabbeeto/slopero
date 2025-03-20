75f82b1c61a5eb21ae720daf3ee4c693dba2f05e
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2025-03-20 18:03:58 -0300

    progress with equation maker

--- src/Content/EquationMaker.ts
+++ src/Content/EquationMaker.ts
@@ -1,3 +1,18 @@
-export default function EquationMaker(): string {
-	return "hello"
+// "(55 / 4)x + 5"
+
+let slope = 1
+
+function detectParenthesis(content: string){
+	const parenthesisRegexString = "\\([0-9 \\+-\\/]+?\\)";
+	//todo if it ends with parenthesis you should definitely include the last item becuse this excludes the last item
+	const parenthesisRegex = new RegExp(`${parenthesisRegexString}[^x]`, 'g');
+	const slopeRegex = new RegExp(`${parenthesisRegexString}x`, 'g');
+	console.log([...content.matchAll(parenthesisRegex)]);
+	console.log(content.match(slopeRegex));
+}
+
+export default function EquationMaker(content: string): string {
+	detectParenthesis(content)
+
+	return content
 }
--- src/Content/Input.tsx
+++ src/Content/Input.tsx
@@ -1,15 +1,15 @@
 import EquationMaker from  './EquationMaker'
 
-function EquationDisplayer(){
+function EquationDisplayer(text: string): void {
 let output = document.querySelector('output');
-output!.innerText = EquationMaker()
+output!.innerText = EquationMaker(text)
 }
 
 export default function InputSection() {
 	return (<><section className="flex flex-col gap-2">
 					<h2>write your equation</h2>
 					<input type="text" className="border-white border-2 p-2 rounded-3xl"/>
-					<button  onClick={EquationDisplayer} className="border-white border-2 rounded-2xl hover:border-black hover:bg-white hover:text-black">apply</button>
+					<button  onClick={() => {EquationDisplayer(document.querySelector('input')!.value)}} className="border-white border-2 rounded-2xl hover:border-black hover:bg-white hover:text-black">apply</button>
 					<output></output>
 	</section>
 	</>)
