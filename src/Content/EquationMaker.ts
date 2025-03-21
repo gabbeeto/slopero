a998a3900829c059583d66fca90dc378278d748d
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2025-03-21 17:25:37 -0300

    start with the last stage of the equation and make it so it can solve when y is divisible

--- src/Content/EquationMaker.ts
+++ src/Content/EquationMaker.ts
@@ -1,18 +1,72 @@
 // "(55 / 4)x + 5"
 
-let slope = 1
+// let slope = 1
 
-function detectParenthesis(content: string){
-	const parenthesisRegexString = "\\([0-9 \\+-\\/]+?\\)";
-	//todo if it ends with parenthesis you should definitely include the last item becuse this excludes the last item
-	const parenthesisRegex = new RegExp(`${parenthesisRegexString}[^x]`, 'g');
-	const slopeRegex = new RegExp(`${parenthesisRegexString}x`, 'g');
-	console.log([...content.matchAll(parenthesisRegex)]);
-	console.log(content.match(slopeRegex));
+//TODO: work on this later on
+//function detectParenthesis(content: string){
+//	const parenthesisRegexString = "\\([0-9 \\+-\\/]+?\\)";
+//	//todo if it ends with parenthesis you should definitely include the last item becuse this excludes the last item
+//	const parenthesisRegex = new RegExp(`${parenthesisRegexString}[^x]`, 'g');
+//	const slopeRegex = new RegExp(`${parenthesisRegexString}x`, 'g');
+//	console.log([...content.matchAll(parenthesisRegex)]);
+//	console.log(content.match(slopeRegex));
+//}
+
+// detectParenthesis(content)
+let currentStageIndex: number = 0;
+const stages: string[] = ['divideOrMultiplyTarget']
+const returns: any[]= []
+
+export default function EquationMaker(content: string): any[] {
+	let currentStage: string = stages[currentStageIndex]
+	if (currentStage == 'divideOrMultiplyTarget') {
+		returns.push([currentStage,  divideOrMultiplyTargetStage(content)])
+
+
+	}
+	return returns
 }
 
-export default function EquationMaker(content: string): string {
-	detectParenthesis(content)
+function divideOrMultiplyTargetStage(content: string): string {
+	let stringNumbers: string[] = content.split("=");
+	const yIsPresent = (element: string) => element.search("y") != -1
+	let indexForY: number = stringNumbers.findIndex(yIsPresent);
+	// turn strings into number
+	let numbers: number[] = stringNumbers.map((e) => {
+		return Number(e.match(/[0-9\.-]+/g)![0])
+	})
 
-	return content
+	// seperate y value so we can compare in the array:
+	// save the y value elsewhere
+	let yValue = numbers[indexForY]
+	// remove y value in array
+	numbers.splice(indexForY, 1)
+
+
+	// because different we gotta see if we get fractions
+	let yIsDivisible: boolean = true
+	for (let number of numbers) {
+		let numberIsNotDivisibleByY: boolean = number % yValue != 0
+		if (numberIsNotDivisibleByY) {
+			yIsDivisible = false
+		}
+	}
+	return yIsDivisible ? `y = ${divideY(yValue, numbers)}` : `y = ${unifyInFractionForm()}`;
+
+
+}
+
+function divideY(yValue: number, numbers: number[]): string {
+	if (numbers.length == 1) {
+		return `${yValue / numbers[0]}`
+	}
+	else {
+		return numbers.map((e: number) => e / yValue).join(" = ")
+	}
+
+}
+
+
+function unifyInFractionForm(): string {
+	return "y not  is divisible"
 }
--- src/Content/Input.tsx
+++ src/Content/Input.tsx
@@ -1,16 +1,17 @@
-import EquationMaker from  './EquationMaker'
+import EquationMaker from './EquationMaker'
 
 function EquationDisplayer(text: string): void {
-let output = document.querySelector('output');
-output!.innerText = EquationMaker(text)
+	for (let container of EquationMaker(text)) {
+		document.querySelector(`#${container[0]}`)!.innerHTML = container[1]
+	}
 }
 
 export default function InputSection() {
 	return (<><section className="flex flex-col gap-2">
-					<h2>write your equation</h2>
-					<input type="text" className="border-white border-2 p-2 rounded-3xl"/>
-					<button  onClick={() => {EquationDisplayer(document.querySelector('input')!.value)}} className="border-white border-2 rounded-2xl hover:border-black hover:bg-white hover:text-black">apply</button>
-					<output></output>
+		<h2>write your equation</h2>
+		<input type="text" className="border-white border-2 p-2 rounded-3xl" />
+		<button onClick={() => {EquationDisplayer(document.querySelector('input')!.value)}} className="border-white border-2 rounded-2xl hover:border-black hover:bg-white hover:text-black">apply</button>
+		<output id='divideOrMultiplyTarget'></output>
 	</section>
 	</>)
 }
